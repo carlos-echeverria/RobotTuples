f730cd24ab45cfd266c11b1c3af40a6633f42154
Carlos Echeverria <echeverriacarlos@gmail.com>
2020-11-10 12:31:52 +0100

    Refactored code: included functions for swapping elements, added documentation for each function

--- robotTuples.py
+++ robotTuples.py
@@ -1,92 +1,156 @@
 #!/usr/bin/env python3
 
-import sys
+# import sys
 from itertools import combinations
-from random import choice
-from random import seed
-from collections import Counter
-
-n = 18 # number of robots
-k = 3  # tuple size
-robots = list(range(1,n+1))
-groupSize = 24
-#seed(6)
+from collections import Counter, defaultdict
+from random import choice, choices, seed, sample
 
 
-def possibleSets(robots,k):
-    # return list of all subsets of length k form the n-element array robots.
+def possibleTuples(robots,k):
+    # Return a list of all tuples of length k form a list of robots of size n.
     return list(combinations(robots,k))
+    #return [frozenset(x) for x in combinations(robots,k)]
 
 
-def addElementToGroup(totalSets,Group,discarded):
-    # add element if constraints are met
-    seed(6)
+def addTupleToGroup(totalTuples,group,discarded):
+    # Given a list of robot 3-tuples and two groups with or without elements,
+    # the function adds a tuple to the group only if the individual robot count
+    # does not exceed 4, i.e., only if the desired constraints are met).
+    # Warning: function assumes 3-tuples and a totla of 18 robots.
     try:
-        rElement = choice(totalSets)
-        #rElement = totalSets[0]
-        print("We choose a random element from the possible 3-tuples: {}.".format(rElement))
+        randTuple = choice(totalTuples)
+        #randTuple = totalSets[0]
+        ##print("We choose a random element from the possible 3-tuples: {}.".format(randTuple))
 
-        Group.append(rElement)
-        GroupL = [x for tup in Group for x in tup]
-        cnt = Counter(GroupL)
+        group.append(randTuple)
+        groupL = [robot for tupl in group for robot in tupl]
+        cnt = Counter(groupL)
         robotsInGroup = list(cnt.keys())
         if (cnt[1]<=4 and cnt[2]<=4 and cnt[3]<=4 and cnt[4]<=4 and cnt[5]<=4 and cnt[6]<=4 and cnt[7]<=4 and cnt[8]<=4 and cnt[9]<=4 and cnt[10]<=4 and cnt[11]<=4 and cnt[12]<=4 and cnt[13]<=4 and cnt[14]<=4 and cnt[15]<=4 and cnt[16]<=4 and cnt[17]<=4 and cnt[18]<=4):
-
             try:
-                idx = totalSets.index(rElement)
-                totalSets.pop(idx)
+                idx = totalTuples.index(randTuple)
+                totalTuples.pop(idx)
             except IndexError:
-                print("error")
+                print("Index error..?")
         else:
-            discarded.append(Group.pop())
-            idx = totalSets.index(rElement)
-            totalSets.pop(idx)
-
+            discarded.append(group.pop())
+            idx = totalTuples.index(randTuple)
+            totalTuples.pop(idx)
 
     except IndexError:
-        print("no more elements in first run")
-        addElementToGroup(discarded, Group, discarded=[])
+        print("No more tuples in first run of list with all possible combinations...")
+        #addTupleToGroup(discarded, Group, discarded=[])
+        #return
 
 
-def generateGroup(groupSize, totalSets, totalCombis):
+def generateGroup(totalTuples, groupSize):
+    # Given a list of robot tuples and a group size, the function generates
+    # a group of tuples of the given size which fulfill the desired constraints,
+    # i.e., each robot appears 4 times in the group.
     iter=0
     group=[]
     discarded=[]
-    while len(group)<groupSize and iter<totalCombis:
+    convergenceFlag=0
+    totalCombis=len(totalTuples)
+    while len(group)<groupSize and iter<totalCombis and convergenceFlag!=1:
         iter=iter+1
         #print("Total Sets Remaining before iteration: {}".format(len(totalSets)))
-        print("Iteration: {}".format(iter))
-        print("Number of Tuples remaining before adding a new element: {}".format(len(totalSets)))
-        print("The Group Before adding a new tuple is:\n {}.".format(group))
-        addElementToGroup(totalSets, group, discarded)
-        print("The Group After adding a new tuple is:\n {}.".format(group))
-        groupList = [x for tup in group for x in tup]
+        ##print("Iteration: {}".format(iter))
+        ##print("Number of Tuples remaining before adding a new element: {}".format(len(totalSets)))
+        ##print("The Group Before adding a new tuple is:\n {}.".format(group))
+        addTupleToGroup(totalTuples, group, discarded)
+        ##print("The Group After adding a new tuple is:\n {}.".format(group))
+        groupList = [robot for tuple in group for robot in tuple]
         robotCounter = Counter(groupList)
-        print("Group Object {}.".format(robotCounter))
-        print("Number of Tuples Remaining after iteration: {}.".format(len(totalSets)))
+        ##print("Group Object {}.".format(robotCounter))
+        ##print("Number of Tuples Remaining after iteration: {}.".format(len(totalSets)))
         #print("Sets Remaining:\n {}".format(totalSets))
-        print("Number of Tuples Discarded so far: {}.".format(len(discarded)))
+        ##print("Number of Tuples Discarded so far: {}.".format(len(discarded)))
         #print("Sets Discarded:\n {}.".format(discarded))
-        print("\n")
+        ##print("\n")
         if (len(group)==groupSize):
-            print("GROUP WAS CREATED WITH THE DESIRED CONSTRAINTS.\n")
-            #totalSets=totalSets+discarded
+            print(f"THE FOLLOWING GROUP WAS CREATED WITH THE DESIRED CONSTRAINTS:\n {sorted(group)}.")
+            convergenceFlag=1
             return(group)
         if (iter==totalCombis):
-            print("\nERROR: A GROUP COULD NOT BE BUILT WITH THE GIVEN SETS.\n")
-            return(group)
+            print("\nERROR: A GROUP COULD NOT BE BUILT WITH THE GIVEN SETS...TRYING AGAIN.\n")
+            totalTuples=totalTuples+group+discarded
+            iter=0
+            group=[]
+            discarded=[]
+            convergenceFlag=0
+            #return(group)
 
 
-totalSets = possibleSets(robots,k)
-totalCombis = len(totalSets)
-
-print("\nWith {} robots, there are {} posible combinations of {}-tuples.\n".format(n, totalCombis, k))
-
-Groups = []
-outit = 0
-while outit<3:
-     outit = outit + 1
-     Groups.append(generateGroup(groupSize, totalSets, totalCombis))
+def countElementsInGroup(group):
+    # Given a group of robots (list of tuples), the function counts the number
+    # of times each robot appears in the group.
+    c = Counter()
+    for t in group:
+        c.update(t)
+    return c
 
 
-print(Groups)
+def isGroupComplete(group, groupSize, nRobots):
+    # Given a group of robots (list of tuples), a group size and the number of
+    # times each robot should appear in the group, the function returns a
+    # true/false statement if the group fulfills the constraints.
+    c = countElementsInGroup(group)
+
+    valid = (len(c) == groupSize)
+    for n in c.values():
+        valid &= (n == nRobots)
+    return valid
+
+
+def pairKey(pair):
+    # Given a pair of tuples, the function returns a key which identifies the
+    # tuples by the members it posseses.
+    return tuple(sorted(list(pair[0]) + list(pair[1])))
+
+
+def buildPairsDict(availableTuples):
+    # Given the list of all available tuples, the function creates a dictionary
+    # of the form:
+    #            key = the set of elements in the two tuples
+    #          value = the list of tuple pairs that correspond to the key
+    #
+    tuplesPairs = defaultdict(list)
+    for pair in combinations(availableTuples, 2):
+        key = pairKey(pair)
+        tuplesPairs[key].append(pair)
+    return tuplesPairs
+
+
+def shufflePairOfElements(currentGroup, allTuples):
+    # Given a tuple group and a list of all available tuples, the function
+    # selects two tuples at random, looks it up in the dictionary, and picks
+    # another pair if there is one
+
+    # Make a copy, I want to avoid modifying the input:
+    currentGroup = set(currentGroup)
+
+    availableTuples = set(allTuples) - set(currentGroup)
+    availableTuplesPairs = buildPairsDict(availableTuples)
+
+    # Pick two tuples at random:
+    replaceTuples = sample(currentGroup, k=2)
+
+    # Pick two tuples with the same elements to replace them:
+    availableMatchingTuples = availableTuplesPairs[pairKey(replaceTuples)]
+    if len(availableMatchingTuples) == 0:
+        # No tuples available: do nothing
+        ###print('No Shuffling was performed in this step')
+        return currentGroup
+
+    newTuples = choice(availableMatchingTuples)
+    print(f"We will replace these tuples: {replaceTuples},")
+    print(f"with these tuples: {newTuples}.")
+
+    # Replace old tuples with new tuples:
+    currentGroup.remove(replaceTuples[0])
+    currentGroup.remove(replaceTuples[1])
+    currentGroup.add(newTuples[0])
+    currentGroup.add(newTuples[1])
+
+    return currentGroup
